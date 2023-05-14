c821a445ce7aedcbc56a770bdb577d6f70bbb211
1211398616@qq.com <1211398616@qq.com>
2023-05-14 19:09:34 +0800

    提交着玩，就是玩儿

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,7 +2,7 @@
 	<view class="content">
 		<image class="logo" src="/static/logo.png"></image>
 		<view class="text-area">
-			<text class="title">{{title}}1</text>
+			<text class="title">{{title}}2</text>
 		</view>
 	</view>
 </template>
